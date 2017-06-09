aa4bc42193ca92e0d6c3092ce563ee090b800446
mark-ocampo <mark.ocampo@rea-group.com>
2017-06-10 02:17:20 +1000

    Add logo to launch screen

--- src/routes/Launch/Launch.js
+++ src/routes/Launch/Launch.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import logo from './yahoo-logo.png';
 import styles from './Launch.scss';
 
 const Launch = () => (
   <div className={styles.launch}>
+    <img className={styles.logo} src={logo} alt="Yahoo Auctions Japan" />
     <h1 className={styles.text}>Launch.js</h1>
   </div>
 );
