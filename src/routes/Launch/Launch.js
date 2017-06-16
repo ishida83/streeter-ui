f826a60baa12ddd22dd913b340aac9ffc023e05e
mark-ocampo <mark.ocampo@rea-group.com>
2017-06-16 18:11:27 +1000

    Update launch text

--- src/routes/Launch/Launch.js
+++ src/routes/Launch/Launch.js
@@ -6,7 +6,7 @@ import styles from './Launch.scss';
 const Launch = () => (
   <div className={styles.launch}>
     <img src={logo} alt="Yahoo Auctions Japan" />
-    <h1>Launch.js</h1>
+    <h1>Streeter UI</h1>
   </div>
 );
 
