79565b169979299f9b23f1d5f7f6377c2297e43e
mark-ocampo <mark.ocampo@rea-group.com>
2017-06-22 22:08:55 +1000

    Fix style for input box and release

--- src/routes/Search/SearchBar/Input/Input.js
+++ src/routes/Search/SearchBar/Input/Input.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import styles from './Input.scss';
 
 const Input = ({ onChange }) => (
-  <div>
+  <div className={styles.container}>
     <input
       className={styles.input}
       placeholder="Search Yahoo Auctions Japan..."
